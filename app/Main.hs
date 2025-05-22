d183f5aec3b5794d90f55e9a512511b29e237196
Kacper Grabczak <kacpergrabczak@gmail.com>
2025-05-22 14:50:36 +0200

    New stack project

---
+++ app/Main.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE DataKinds #-}
+
+module Main where
+
+import Network.Wai (Application)
+import Network.Wai.Handler.Warp (run)
+import Servant
+
+-- Define the API type
+type API = Get '[JSON] String
+
+-- Implement the server
+server :: Server API
+server = return "Hello, World!"
+
+-- Application
+app :: Application
+app = serve (Proxy :: Proxy API) server
+
+-- Main function
+main :: IO ()
+main = do
+  putStrLn "Running on http://localhost:8080"
+  run 8080 app
