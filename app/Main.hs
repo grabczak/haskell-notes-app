1ba2da26993dbe8cc3f6c13c7add0e29afe542b4
Kacper Grabczak <kacpergrabczak@gmail.com>
2025-08-09 22:10:00 +0200

    Add login endpoint

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.Aeson (ToJSON)
+import Data.Aeson (ToJSON, toJSON)
+import qualified Data.Map as Map
+import qualified Data.Text as T
 import Data.Text.Lazy (Text)
 import qualified Data.Text.Lazy as TL
+import Data.Time.Clock.POSIX (getPOSIXTime)
 import Database.SQLite.Simple (
   FromRow (..),
   Only (Only),
@@ -16,6 +19,7 @@ import Database.SQLite.Simple (
   query_,
  )
 import GHC.Generics (Generic)
+import Web.JWT (ClaimsMap (..), JWTClaimsSet (..), claims, encodeSigned, hmacSecret, secondsSinceEpoch, stringOrURI)
 import Web.Scotty (ActionM, get, json, liftIO, pathParam, post, scotty, text)
 
 data User = User
@@ -63,3 +67,32 @@ main = do
       if null _users
         then text "User created successfully."
         else text "User already exists."
+
+    get "/login/:email/:password" $ do
+      _email <- pathParam "email" :: ActionM Text
+      _password <- pathParam "password" :: ActionM Text
+      _users <- liftIO $ do
+        _conn <- open "simple.db"
+        _users <- query _conn "SELECT * FROM users WHERE email = ? AND password = ?" (_email, _password) :: IO [User]
+        close _conn
+        return _users
+      case _users of
+        [] -> text "Invalid email or password."
+        user : _ -> do
+          if _password == password user
+            then do
+              token <- liftIO $ generateToken user
+              json $ Map.fromList [("token" :: String, token)]
+            else text "Invalid email or password."
+
+jwtSecret :: T.Text
+jwtSecret = "super-secret-key"
+
+generateToken :: User -> IO T.Text
+generateToken user = do
+  let claimsSet =
+        mempty
+          { unregisteredClaims = ClaimsMap $ Map.fromList [("user_id", toJSON (Main.id user))]
+          }
+      key = hmacSecret jwtSecret
+  return $ encodeSigned key mempty claimsSet
