ce77947893fb59b521991185d73076751436622f
Kacper Grabczak <kacpergrabczak@gmail.com>
2025-08-09 19:29:18 +0200

    Add example scotty server

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,49 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-module Main (main) where
+import Data.Aeson (ToJSON)
+import Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy as TL
+import Database.SQLite.Simple (
+  FromRow (..),
+  Only (Only),
+  close,
+  execute,
+  execute_,
+  field,
+  open,
+  query_,
+ )
+import GHC.Generics (Generic)
+import Web.Scotty (get, json, liftIO, pathParam, post, scotty, text)
 
-import Network.Wai.Handler.Warp (run)
-import Servant
+-- Model
+data User = User Int Text deriving (Show, Generic)
+instance FromRow User where
+  fromRow = User <$> field <*> field
+instance ToJSON User
 
--- Define the API type
-type API = Get '[JSON] String
-
--- Implement the server
-server :: Server API
-server = return "Hello, World!"
-
--- Application
-app :: Application
-app = serve (Proxy :: Proxy API) server
-
--- Main function
 main :: IO ()
 main = do
-  putStrLn "Running on http://localhost:8080"
-  run 8080 app
+  conn <- open "simple.db"
+  execute_ conn "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)"
+  close conn
+
+  scotty 8080 $ do
+    -- GET /users - list all users
+    get "/users" $ do
+      users <- liftIO $ do
+        _conn <- open "simple.db"
+        us <- query_ _conn "SELECT id, name FROM users" :: IO [User]
+        close _conn
+        return us
+      json users
+
+    -- POST /users/:name - insert a new user
+    post "/users/:name" $ do
+      name <- pathParam "name"
+      liftIO $ do
+        _conn <- open "simple.db"
+        execute _conn "INSERT INTO users (name) VALUES (?)" (Only (name :: Text))
+        close _conn
+      text ("Added user: " `TL.append` name)
