4b2a5b9cb162e4c67f8bf3e2e91c8a0cbfb9fe65
Kacper Grabczak <kacpergrabczak@gmail.com>
2025-08-12 18:43:42 +0200

    Split code

--- app/Main.hs
+++ app/Main.hs
@@ -1,90 +1,17 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE TypeOperators #-}
 
 module Main where
 
-import Control.Monad.IO.Class
-import Data.Aeson
-import qualified Data.ByteString.Lazy.Char8 as BSC
-import Database.SQLite.Simple hiding ((:.))
-import GHC.Generics
+import Database.SQLite.Simple (close, execute_, open)
+import Lib (API, login, notes, register)
 import Network.Wai.Handler.Warp (run)
 import Servant
 import Servant.Auth.Server
 
-data User = User
-  { userId :: Int
-  , userName :: String
-  }
-  deriving (Eq, Show, Generic, FromRow, FromJWT, FromJSON, ToJWT, ToJSON)
-
-getUser :: Int -> IO (Maybe User)
-getUser _userId = do
-  conn <- open "simple.db"
-  res <- query conn "SELECT * FROM users WHERE userId = ?" (Only _userId) :: IO [User]
-  close conn
-  return $ case res of
-    [] -> Nothing
-    (x : _) -> Just x
-
-type API auths =
-  "register"
-    :> ReqBody '[JSON] User
-    :> Post
-        '[JSON]
-        String
-    :<|> "login"
-      :> ReqBody '[JSON] User
-      :> Post
-          '[JSON]
-          ( Headers
-              '[ Header "Set-Cookie" SetCookie
-               , Header "Set-Cookie" SetCookie
-               ]
-              String
-          )
-    :<|> Auth auths User
-      :> "notes"
-      :> Get '[JSON] String
-
 server :: CookieSettings -> JWTSettings -> Server (API auths)
-server cookieSettings jwtSettings = registerHandler :<|> loginHandler cookieSettings jwtSettings :<|> notesHandler
-
-loginHandler ::
-  CookieSettings ->
-  JWTSettings ->
-  User ->
-  Handler (Headers '[Header "Set-Cookie" SetCookie, Header "Set-Cookie" SetCookie] String)
-loginHandler cookieSettings jwtSettings User{..} = do
-  mUser <- liftIO $ getUser userId
-  case mUser of
-    Nothing -> throwError $ err401{errBody = "User not found"}
-    Just user -> do
-      mLoginAccepted <- liftIO $ acceptLogin cookieSettings jwtSettings user
-      case mLoginAccepted of
-        Nothing -> throwError $ err401{errBody = "Login failed"}
-        Just x -> do
-          eJWT <- liftIO $ makeJWT user jwtSettings Nothing
-          case eJWT of
-            Left _ -> throwError $ err401{errBody = "JWT creation failed"}
-            Right r -> return $ x (BSC.unpack r)
-
-notesHandler :: AuthResult User -> Handler String
-notesHandler (Authenticated User{..}) = return $ "Hello " <> userName <> "!"
-notesHandler _ = throwError err400{errBody = "Authentication required"}
-
-registerHandler :: User -> Handler String
-registerHandler User{..} = do
-  liftIO $ do
-    conn <- open "simple.db"
-    execute conn "INSERT INTO users (userId, userName) VALUES (?, ?)" (userId, userName)
-    close conn
-  return "User registered successfully"
+server cookieSettings jwtSettings = register :<|> login cookieSettings jwtSettings :<|> notes
 
 main :: IO ()
 main = do
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,1 +1,83 @@
-module Lib () where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Lib (API, register, login, notes) where
+
+import Control.Monad.IO.Class
+import Data.Aeson
+import qualified Data.ByteString.Lazy.Char8 as BSC
+import Database.SQLite.Simple hiding ((:.))
+import GHC.Generics
+import Servant
+import Servant.Auth.Server
+
+data User = User
+  { userId :: Int
+  , userName :: String
+  }
+  deriving (Eq, Show, Generic, FromRow, FromJWT, FromJSON, ToJWT, ToJSON)
+
+type API auths =
+  "register"
+    :> ReqBody '[JSON] User
+    :> Post
+        '[JSON]
+        String
+    :<|> "login"
+      :> ReqBody '[JSON] User
+      :> Post
+          '[JSON]
+          ( Headers
+              '[ Header "Set-Cookie" SetCookie
+               , Header "Set-Cookie" SetCookie
+               ]
+              String
+          )
+    :<|> Auth auths User
+      :> "notes"
+      :> Get '[JSON] String
+
+getUserById :: Int -> IO (Maybe User)
+getUserById _userId = do
+  conn <- open "simple.db"
+  res <- query conn "SELECT * FROM users WHERE userId = ?" (Only _userId) :: IO [User]
+  close conn
+  return $ case res of
+    [] -> Nothing
+    (x : _) -> Just x
+
+register :: User -> Handler String
+register User{..} = do
+  liftIO $ do
+    conn <- open "simple.db"
+    execute conn "INSERT INTO users (userId, userName) VALUES (?, ?)" (userId, userName)
+    close conn
+  return "User registered successfully"
+
+login ::
+  CookieSettings ->
+  JWTSettings ->
+  User ->
+  Handler (Headers '[Header "Set-Cookie" SetCookie, Header "Set-Cookie" SetCookie] String)
+login cookieSettings jwtSettings User{..} = do
+  mUser <- liftIO $ getUserById userId
+  case mUser of
+    Nothing -> throwError $ err401{errBody = "User not found"}
+    Just user -> do
+      mLoginAccepted <- liftIO $ acceptLogin cookieSettings jwtSettings user
+      case mLoginAccepted of
+        Nothing -> throwError $ err401{errBody = "Login failed"}
+        Just x -> do
+          eJWT <- liftIO $ makeJWT user jwtSettings Nothing
+          case eJWT of
+            Left _ -> throwError $ err401{errBody = "JWT creation failed"}
+            Right r -> return $ x (BSC.unpack r)
+
+notes :: AuthResult User -> Handler String
+notes (Authenticated User{..}) = return $ "Hello " <> userName <> "!"
+notes _ = throwError err400{errBody = "Authentication required"}
